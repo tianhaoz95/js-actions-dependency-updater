23050578aa5dfbee151394d8972f94c259a72d72
Tianhao Zhou <tianhaoz@umich.edu>
2020-02-27 10:44:21 -0800

    dev: init the function skeleton (#4)

---
+++ index.js
@@ -0,0 +1,11 @@
+const initActionContext = require("./lib/init");
+const upgradeNodeModules = require("./lib/upgrade");
+const maybeOpenPullRequest = require("./lib/pull");
+
+async function main() {
+  const actionContext = await initActionContext();
+  await upgradeNodeModules(actionContext);
+  await maybeOpenPullRequest(actionContext);
+}
+
+main();
